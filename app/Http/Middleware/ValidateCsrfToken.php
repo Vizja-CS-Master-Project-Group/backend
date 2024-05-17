a741496ed14d4ca2fc1f1287c6c4e397ae8234b9
melih <dev.mberatsanli@gmail.com>
2024-05-18 01:10:23 +0200

    csrf disabled

--- app/Http/Middleware/ValidateCsrfToken.php
+++ app/Http/Middleware/ValidateCsrfToken.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken as BaseVerifier;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
-class ValidateCsrfToken extends BaseVerifier
+class ValidateCsrfToken extends VerifyCsrfToken
 {
     protected $except = [
         '*',
